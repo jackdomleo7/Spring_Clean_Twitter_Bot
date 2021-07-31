2159af676376183b6eb27437d44053aecadd59b0
Jack Domleo <jackdomleo7@gmail.com>
2021-07-31 14:09:54 +0100

    Fix Twitter pagination issue and add additional logging

--- src/bot.ts
+++ src/bot.ts
@@ -29,8 +29,14 @@ async function getTwitterFriends(): Promise<Record<string, any>[] | undefined> {
   try {
     const response = await Twitter.accountsAndUsers.accountSettings()
     const screen_name = response.screen_name
-    const friends = await Twitter.accountsAndUsers.friendsList({ screen_name })
-    return friends.users
+    let friends = await Twitter.accountsAndUsers.friendsList({ screen_name, count: 200 })
+    let users = friends.users
+    while (friends.next_cursor !== 0) {
+      friends = await Twitter.accountsAndUsers.friendsList({ screen_name, count: 200, cursor: friends.next_cursor })
+      users = [...users, ...friends.users]
+    }
+    console.log(`${users.length} Twitter friends found`)
+    return users
   }
   catch(e: any) {
     console.error(e)
@@ -39,6 +45,8 @@ async function getTwitterFriends(): Promise<Record<string, any>[] | undefined> {
 }
 
 function unfollowInactiveUsers(friends: Record<string, any>[], whitelistedHandles: string[]): void {
+  let friendsUnfollowed = 0
+
   friends.forEach(async (friend: Record<string, any>) => {
     if (!whitelistedHandles.includes(friend.screen_name)) {
       const criteria: ICriteria = {
@@ -50,38 +58,24 @@ function unfollowInactiveUsers(friends: Record<string, any>[], whitelistedHandle
       }
       else {
         await Twitter.accountsAndUsers.friendshipsDestroy({ screen_name: friend.screen_name })
+        friendsUnfollowed++
         console.log(`Unfollowing @${friend.screen_name} (${friend.name}) because they are not a whitelisted user, ${showUnmetCriteria(criteria)}`)
       }
     } else {
       console.log(`Skipping @${friend.screen_name} (${friend.name}) because they are a whitelisted user`)
     }
   })
+
+  console.log(`${friendsUnfollowed} of ${friends.length} friends unfollowed (you are now following ${friends.length - friendsUnfollowed} accounts)`)
 }
 
 // Start of script
 (async function(){
-  /*******************************/
-  /* START GET WHITELISTED USERS */
-  /*******************************/
-
   const whitelistedHandles = await getWhitelistedHandles()
   if (!whitelistedHandles) process.exit() // Terminate the script if a connection to the GitHub gist cannot be established - otherwise we will have no list of whitelisted users and may accidentally unfollow a whitelisted user
 
-  /*****************************/
-  /* END GET WHITELISTED USERS */
-  /*****************************/
-
-
-  /*************************************************/
-  /* START GET TWITTER FRIENDS (PEOPLE YOU FOLLOW) */
-  /*************************************************/
-
   const twitterFriends = await getTwitterFriends()
   if (!twitterFriends) process.exit() // Terminate the script if we cannot get a list of friends from Twitter - no point in running the script anymore
-
-  /***********************************************/
-  /* END GET TWITTER FRIENDS (PEOPLE YOU FOLLOW) */
-  /***********************************************/
 
   unfollowInactiveUsers(twitterFriends, whitelistedHandles)
 })()
